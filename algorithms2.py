d9fdc8116da8c316b6852148274d03a19b3cd5b0
Miradin <37274960+Miradin@users.noreply.github.com>
2018-08-24 13:44:39 +0200

    added some comments

--- algorithms2.py
+++ algorithms2.py
@@ -5,17 +5,17 @@
 
 people = 5;
 groups = 4;
-a = str(people)*groups
-b = int(a)
-for j in range(b+1):
+a = str(people)*groups #make string 5555
+b = int(a) #will be used as max number for cycle
+for j in range(b):
     n = j
-    result = 0
-    for i in range(len(str(n))):
-        result = result + int(str(n)[i:i+1])
+    summ = 0 #getting summ of digits
+    for i in range(len(str(n))): 
+        summ = summ + int(str(n)[i:i+1])
 
-    if (result==people):
-        t = '{:0>{groups}d}'.format(j, groups = groups)
+    if (summ==people):
+        t = '{:0>{groups}d}'.format(j, groups = groups) #formating output
         print("Could be obtained as summ of: ")
         for k in range (groups):
-            print(t[k] + "  ", end='')
+            print(t[k] + "  ", end='') #print without endline
         print()
