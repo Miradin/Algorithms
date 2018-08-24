ef06ab3bf25fb53d578df73138773b481dacb77b
Miradin <37274960+Miradin@users.noreply.github.com>
2018-08-24 13:27:34 +0200

    Create algorithms2.py

---
+++ algorithms2.py
@@ -0,0 +1,20 @@
+#Теперь возьмём задачу, где люди идентичны, а группы уникальны. 
+#То есть надо просто какую-то группу людей разделить на команды. Пустые команды допускаются.
+# Проще всего реализовать на Питоне из-за легкого процесса конвертаций типов.
+
+people = 5;
+groups = 4;
+a = str(people)*groups
+b = int(a)
+for j in range(b+1):
+    n = j
+    result = 0
+    for i in range(len(str(n))):
+        result = result + int(str(n)[i:i+1])
+
+    if (result==people):
+        t = '{:0>{groups}d}'.format(j, groups = groups)
+        print("Could be obtained as summ of: ")
+        for k in range (groups):
+            print(t[k] + "  ", end='')
+        print()
